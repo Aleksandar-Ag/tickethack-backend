8fe0c3edc142fab60e8ae1541b0ac05b1362127f
Aleksandar <gvozdenovic.aleksandre@gmail.com>
2025-03-11 17:18:09 +0100

    backend v3

--- routes/trips.js
+++ routes/trips.js
@@ -5,16 +5,7 @@ const moment = require('moment');
 let data = require('../trips.json')
 
 router.post('/', (req, res) => {
-    for (let tr of data) {
-        let date = new Date(tr.date.$date)
-        let format1 = "DD-MM-YYYY"
-        date = moment(date).format(format1)
-    }
 
-        
-
-    /*let traj = data.filter(trajet => (trajet.departure === req.body.departure) 
-    && (trajet.arrival === req.body.arrival)  )*/
-})
+});
 
 module.exports = router
