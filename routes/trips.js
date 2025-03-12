587f06311776b7ef9946b0ca7e0c7cc09ab17451
Aleksandar <gvozdenovic.aleksandre@gmail.com>
2025-03-12 11:49:26 +0100

    Ajout var BDD trips

--- routes/trips.js
+++ routes/trips.js
@@ -1,3 +1,6 @@
+require('../models/connection');
+const Trajet = require('../models/trajets')
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,20 +8,25 @@ const moment = require('moment');
 let data = require('../trips.json')
 
 router.post('/', (req, res) => {
-    const trajet = data.filter(traj => (traj.departure === req.body.departure)
-    && (traj.arrival === req.body.arrival)
-    && (moment(traj.date.$date).format(DD-MM-YYYY) === req.body.date))
+    // function dateFormatted(jour) {
+        
+    // }
+    const date = new Date(req.body.date)
 
+    
     if (!req.body.departure || !req.body.arrival || !req.body.date) {
         res.json({ error: "No trip found" });
         return;
     }
-
+    
     if (trajet) {
         res.json({ result: trajet });
         return;
     }
-
+    
+    const trajet = data.filter(traj => (traj.departure === req.body.departure)
+    && (traj.arrival === req.body.arrival)
+    /*&& (moment(traj.date.$date).format(DD-MM-YYYY) === req.body.date)*/)
 });
 
 module.exports = router
